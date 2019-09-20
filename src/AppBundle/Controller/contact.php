17b9232c8b8d5b71f55655f1e4e6f2c774af8a89
Piotr Prusaczyk <piter0105@wp.pl>
2019-09-20 13:44:57 +0200

    visual theme finall

---
+++ src/AppBundle/Controller/contact.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class contact extends Controller
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+
+    public function SetRandomData()
+    {
+        $name = $this->GetRandomName();
+        $surname = $this->GetRandomSurname();
+        $street = $this->GetRandomStreet();
+        $city = $this->GetRandomCity();
+        $phoneNumber = $this->GetRandomPhoneNumber();
+
+        return $this->render('contact.html.twig', ['name' => $name, 'surname' => $surname, 'street' => $street, 'city' => $city, 'phoneNumber' => $phoneNumber]);
+    }
+
+    private function GetRandomName()
+    {
+        return 'Daniel';
+    }
+
+    private function GetRandomSurname()
+    {
+        return 'Nowak';
+    }
+
+    private function GetRandomStreet()
+    {
+        return 'Nowa 24';
+    }
+
+    private function GetRandomCity()
+    {
+        return 'Poznań';
+    }
+
+    private function GetRandomPhoneNumber()
+    {
+        return 1123789456;
+    }
+
+}
